4a71638b5ab2b208290e5ee5a7eabce07a906682
SandraT <sndrbltr@gmail.com>
2021-08-31 17:15:33 +0000

    MySQL

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // minimalistine logika, kaip paleisti projekta
-
+const mysql = require('mysql2/promise');
 const server = require('./lib/server');
 
 const app = {}    //objektas app
 
-app.init = () => {       // inicijuojame app
+app.init = async () => {       // inicijuojame app
+    // prisijungti prie duomenu bazes
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'barsukas',
+    });
+
     // paruosti reikiamas direktorijas
 
     // paruosti reikiamus failus
 
     // inicijuojame serveri
-    server.init();   // kreipiuosi i server objekta ir paleidziu init metoda
+    server.init(connection);   // kreipiuosi i server objekta ir paleidziu init metoda
 }
 
 app.init();
