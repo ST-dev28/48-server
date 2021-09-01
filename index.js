5a5201c5f6a07061f24387eb45645f40155b3262
SandraT <sndrbltr@gmail.com>
2021-09-01 17:37:54 +0000

    Without database part

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 // minimalistine logika, kaip paleisti projekta
-const mysql = require('mysql2/promise');
 const server = require('./lib/server');
 
 const app = {}    //objektas app
 
-app.init = async () => {       // inicijuojame app
-    // prisijungti prie duomenu bazes
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        database: 'barsukas',
-    });
+app.init = () => {       // inicijuojame app
 
     // paruosti reikiamas direktorijas
 
     // paruosti reikiamus failus
 
     // inicijuojame serveri
-    server.init(connection);   // kreipiuosi i server objekta ir paleidziu init metoda
+    server.init();   // kreipiuosi i server objekta ir paleidziu init metoda
 }
 
 app.init();
